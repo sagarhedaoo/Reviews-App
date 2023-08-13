7d1970c5c0ba5a2748c7c859de63210e63d44ab7
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-08-13 17:16:07 -0400

    Code Cleanup

--- App.js
+++ App.js
@@ -1,18 +1,8 @@
 import React, { useState } from "react";
 import Home from "./screens/home";
-// import * as Font from "expo-font";
-// import { AppLoading } from "expo";
 import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 
-// const getFonts = () =>
-//   Font.loadAsync({
-//     "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
-//     "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
-//   });
-
 export default function App() {
-  // const [fontsLoaded, setFontsLoaded] = useState(false);
-
   let [fontsLoaded, fontError] = useFonts({
     Inter_900Black,
   });
