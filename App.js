4a5d2d14cee8cb6f01f10f61a9e7f9722182f1f1
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-08-13 17:15:28 -0400

    Added Global Styles and Fixed Fonts error

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import React, { useState } from "react";
 import Home from "./screens/home";
-import * as Font from "expo-font";
-import { AppLoading } from "expo";
+// import * as Font from "expo-font";
+// import { AppLoading } from "expo";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 
-const getFonts = () =>
-  Font.loadAsync({
-    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
-    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
-  });
+// const getFonts = () =>
+//   Font.loadAsync({
+//     "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+//     "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+//   });
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+  // const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  if (fontsLoaded) {
-    return <Home />;
-  } else {
-    return (
-      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
-    );
+  let [fontsLoaded, fontError] = useFonts({
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
   }
+
+  return <Home />;
 }
---
+++ styles/global.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+  },
+  titleText: {
+    fontSize: 18,
+    fontFamily: "Inter_900Black",
+    color: "#333",
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+});
