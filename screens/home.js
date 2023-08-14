0743ea12bd935f380fca72763fc62a1e908fbb09
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-08-14 08:45:41 -0400

    Created useState Dummy

--- screens/home.js
+++ screens/home.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
 export default function Home({ navigation }) {
-  const pressHandler = () => {
-    navigation.navigate("ReviewDetails");
-    //navigation.push("ReviewDetails");
-  };
+  const [reviews, setReviews] = useState([
+    { title: "Hi This is Title 1", rating: 5, body: "Something", key: "1" },
+    { title: "Hi This is Title 2", rating: 4, body: "Something", key: "2" },
+    { title: "Hi This is Title 3", rating: 3, body: "Something", key: "3" },
+  ]);
 
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home Screen</Text>
-      <Button title="Go to Review Details" onPress={pressHandler} />
     </View>
   );
 }
