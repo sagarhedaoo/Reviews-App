05e034a78db3e9d402f203b53fed1e9b7b4c24d7
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-08-14 08:59:49 -0400

    Navigation Options added

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -6,12 +6,25 @@ import ReviewDetails from "../screens/reviewDetails";
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: {
+      title: "My Review App",
+      //   headerStyle: { backgroundColor: "#eee" },
+    },
   },
   ReviewDetails: {
     screen: ReviewDetails,
+    navigationOptions: {
+      title: "Review Details",
+      //   headerStyle: { backgroundColor: "#eee" },
+    },
   },
 };
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: "red",
+    headerStyle: { backgroundColor: "#eee" },
+  },
+});
 
 export default createAppContainer(HomeStack);
