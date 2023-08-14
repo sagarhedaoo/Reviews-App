1e98fa9062d56fbd771634566ed05bf5b4bcf9a4
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-08-14 08:36:39 -0400

    Added Button to Functionality to navigate from Home Screen

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function Home() {
+export default function Home({ navigation }) {
+  const pressHandler = () => {
+    navigation.navigate("ReviewDetails");
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home Screen</Text>
+      <Button title="Go to Review Details" onPress={pressHandler} />
     </View>
   );
 }
